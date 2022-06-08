46aad8a919275f5572bee3e341e5ae5218f6acb1
Gokulcasper <ronitcasper215@gmail.com>
2022-06-08 14:50:02 +0530

    Feat: sort,filter,map

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ const item = [
   { id: 3, mobile: "Desktop", cost: 10000 },
 ];
 
-// // Sort it using lowest price
+// // --------Sort it using lowest price-------------
 
 // let itemCost = item.sort((a, b) => {
 //   return a.cost - b.cost; // ascending
 //   // return b.cost - a.cost; // descending
 // });
 
-// // Sort it title ascending
+// // ---------Sort it title ascending----------------
 
 // let itemMobile = itemCost.sort((c, d) => {
 //   if (c.mobile < d.mobile) return -1;
@@ -19,30 +19,30 @@ const item = [
 //   return 1;
 // });
 
-// // Filter Product less than 11000
+// // -----------Filter Product less than 11000-------
 
 // let itemFilter = itemMobile.filter((value) => {
 //   return value.cost < 11000;
 // });
 
-// // Map // Android Phone - ₹10000// &#8377;
+// //------- Map // Android Phone - ₹10000// &#8377;-------
 
 // let itemMap = itemFilter.map((val) => {
 //   return val.mobile + "  -  ₹" + val.cost;
 // });
 // console.log(itemMap);
 
+//--------------------------------------------------------
+
+// Code SORT
+
 let itemMap = item
-  .sort((a, b) => {})
+  .sort((a, b) => a.cost - b.cost)
   .sort((c, d) => {
     if (c.mobile < d.mobile) return -1;
     if (c.mobile > d.mobile) return 1;
     return 1;
   })
-  .filter((value) => {
-    return value.cost < 11000;
-  })
-  .map((val) => {
-    return val.mobile + "  -  ₹" + val.cost;
-  });
+  .filter((value) => value.cost < 11000)
+  .map((val) => val.mobile + "  -  ₹" + val.cost);
 console.log(itemMap);
